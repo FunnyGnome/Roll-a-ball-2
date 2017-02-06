d6cc2120ed6ccd5da853c1cdc309186a3906f342
FunnyGnome <thomasmorris00@gmail.com>
2017-02-06 13:43:29 -0700

    Maxed Movement Speed

--- 3d/Assets/Scripts/PlayerController.cs
+++ 3d/Assets/Scripts/PlayerController.cs
@@ -7,19 +7,25 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public Text countText;
     public Text winText;
-
-    private Rigidbody rb;
+    public float maxSpeed;
     private int count;
     private float jumpTime;
+    public Rigidbody rb;
+    public float maxBoost;
+    Vector3 v3Velocity;
+
+
+
     void Start ()
     {
         rb = GetComponent<Rigidbody>();
+        v3Velocity = rb.velocity;
         count = 0;
         SetCountText();
         winText.text = "";
     }
 
-    void FixedUpdate ()
+    void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
@@ -28,9 +34,37 @@ public class PlayerController : MonoBehaviour {
 
         if (moveVertical > 0)
         {
-            rb.AddForce(movement * speed*2);
+            var movementX = movement.x;
+            v3Velocity = rb.velocity;
+            if (v3Velocity.x > maxBoost || v3Velocity.x < maxBoost * -1)
+            {
+                Debug.Log("We're goin too fast. " + v3Velocity);
+            }
+            else
+            {
+                //var speedBoost = Mathf.Sqrt(movement*  speed*2);
+                rb.AddForce(movement * speed * 2);
+                Debug.Log("Gotta go fast. " + v3Velocity);
+            }
         }
-        rb.AddForce(movement * speed);
+        else
+        {
+            var movementX = movement.x;
+            v3Velocity = rb.velocity;
+            if (v3Velocity.x > maxSpeed || v3Velocity.x < maxSpeed * -1)
+            {
+                Debug.Log("We're goin too fast. " + v3Velocity);
+            }
+            else
+            {
+                //var speedBoost = Mathf.Sqrt(movement*  speed*2);
+                rb.AddForce(movement * speed * 2);
+                Debug.Log("Gotta go fast. " + v3Velocity);
+            }
+
+
+
+        }
     }
 
     void OnTriggerEnter(Collider other)
