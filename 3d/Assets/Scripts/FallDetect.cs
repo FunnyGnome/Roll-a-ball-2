58bc421e2edc5f3d5f784af9b46faacb77f57cbd
FunnyGnome <thomasmorris00@gmail.com>
2017-02-16 14:00:27 -0700

    Scripts
    Changes to scripts

---
+++ 3d/Assets/Scripts/FallDetect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallDetect : MonoBehaviour {
+    GameObject player;
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (player.transform.position.y <= -80)
+        {
+            player.transform.position = new Vector3(0, 1, 0);
+        }
+	}
+}
